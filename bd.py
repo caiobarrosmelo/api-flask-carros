3cae7dd73c95abbc774fc0f7899cb7dd268e16b7
Caio <163363027+caiobarrosmelo@users.noreply.github.com>
2025-05-08 18:23:43 -0300

    Add files via upload
    API simples utilizando Python e Flask com os métodos POST e GET em menos de 30 linhas de código.

---
+++ bd.py
@@ -0,0 +1,34 @@
+carros = [
+    {
+        "id": 1,
+        "marca": "Fiat",
+        "modelo": "Fiat Uno",
+        "ano": 1995,
+    },
+    {
+        "id": 2,
+        "marca": "Volkswagen",
+        "modelo": "Gol",
+        "ano": 2000,
+    },
+    {
+        "id": 3,
+        "marca": "Chevrolet",
+        "modelo": "Chevette",
+        "ano": 1973,
+    },
+    {
+        "id": 4,
+        "marca": "Ford",
+        "modelo": "Escort",
+        "ano": 1986,
+    },
+    {
+        "id": 5,
+        "marca": "Fiat",
+        "modelo": "Marea",
+        "ano": 2000,
+    },
+]
+
+print(carros)
